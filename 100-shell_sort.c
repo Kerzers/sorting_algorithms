1dbd83aadcbe5ddd6d8f46f64b61dec41eabeb30
Jealous Matsikachando <pasej5@gmail.com>
2023-07-20 22:07:48 -0700

    function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		size_t i, j;
+		int temp;
+
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+
+			array[j] = temp;
+		}
+
+		print_array(array, size);
+
+		gap = (gap - 1) / 3;
+	}
+}
+
